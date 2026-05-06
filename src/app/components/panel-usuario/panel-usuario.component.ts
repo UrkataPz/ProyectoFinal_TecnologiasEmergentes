9399295b4c2eb1155fbb377daabb6427c566b98c
Johan Peraza <johanperaza039@gmail.com>
2026-05-05 18:39:28 -0600

    Nuevos Grid para el Panel de usuario
    se agregaron grids para mostrar y modificar la información del usuario.

--- src/app/components/panel-usuario/panel-usuario.component.ts
+++ src/app/components/panel-usuario/panel-usuario.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-panel-usuario',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './panel-usuario.component.html',
   styleUrl: './panel-usuario.component.css'
 })
 export class PanelUsuarioComponent {
+  // Información del usuario por defecto
+  usuario = {
+    nombre: 'Juan Pérez',
+    correo: 'juan.perez@unitec.edu',
+    carrera: 'Ingeniería en Sistemas',
+    rol: 'Estudiante'
+  };
+
   solicitudes = [
     { club: 'Club de Tecnología e Innovación', estado: 'Pendiente', fecha: '05/05/2026' },
     { club: 'Club de Voluntariado', estado: 'Aprobada', fecha: '04/05/2026' },
